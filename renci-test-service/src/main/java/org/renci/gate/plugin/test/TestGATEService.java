bf07e8f26cb4172d88363246a783c75bc4a1d406
Jason Reilly <jdr0887@renci.org>
2013-03-27 16:03:55 -0400

    initial commit

---
+++ renci-test-service/src/main/java/org/renci/gate/plugin/test/TestGATEService.java
@@ -0,0 +1,47 @@
+package org.renci.gate.plugin.test;
+
+import java.util.Map;
+
+import org.renci.gate.AbstractGATEService;
+import org.renci.gate.GlideinMetric;
+import org.renci.jlrm.Queue;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 
+ * @author jdr0887
+ */
+public class TestGATEService extends AbstractGATEService {
+
+    private final Logger logger = LoggerFactory.getLogger(TestGATEService.class);
+
+    public TestGATEService() {
+        super();
+    }
+
+    @Override
+    public Map<String, GlideinMetric> lookupMetrics() {
+        return null;
+    }
+
+    @Override
+    public void createGlidein(Queue queue) {
+        logger.info("ENTERING createGlidein(Queue)");
+        if (!getActiveQueues().contains(queue.getName())) {
+            logger.warn("queue name is not in active queue list...see etc/org.renci.gate.plugin.killdevil.cfg");
+            return;
+        }
+    }
+
+    @Override
+    public void deleteGlidein(Queue queue) {
+        logger.info("ENTERING deleteGlidein(Queue)");
+
+    }
+
+    @Override
+    public void deletePendingGlideins() {
+    }
+
+}
